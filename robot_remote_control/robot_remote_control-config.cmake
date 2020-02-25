76f3ec39a82d6eefd40f2df47e252a2de5a7ec73
Steffen Planthaber <planthaber@SPLANTHABER-U.dfki.uni-bremen.de>
2020-02-25 11:25:53 +0100

    folder rename

---
+++ robot_remote_control/robot_remote_control-config.cmake
@@ -0,0 +1,10 @@
+
+
+find_package(Protobuf REQUIRED)
+
+find_package(PkgConfig)
+pkg_check_modules(ZMQ REQUIRED libzmq)
+
+
+include("${CMAKE_CURRENT_LIST_DIR}/robot_remote_control-targets.cmake")
+
