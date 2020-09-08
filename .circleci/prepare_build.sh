8852745e4223e5cca238a85c071a81fdb7b16a6d
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2020-09-08 10:46:54 +0200

    Remove rock stuff from circleci config

--- .circleci/prepare_build.sh
+++ .circleci/prepare_build.sh
@@ -2,8 +2,6 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y build-essential git cmake pkg-config libboost-test-dev libboost-system-dev libprotobuf-dev protobuf-compiler libzmqpp-dev
-./install_source_dependencies.sh ./external
-. env.sh 
 mkdir build
 cd build
 cmake $@ ..
