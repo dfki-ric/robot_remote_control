445819770563b5d4ad3332ddb6e0cf7acf1d169a
Steffen Planthaber <steffen.planthaber@dfki.de>
2021-07-13 14:14:12 +0200

    don't provide global include file for conversions

--- src/Types/Conversions/rock/rock.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Conversions/Eigen.hpp"
-#include "Conversions/IMUSensors.hpp"
-#include "Conversions/Joints.hpp"
-#include "Conversions/Pose.hpp"
-#include "Conversions/SubTrajectory.hpp"
