ae96994bab6f21b2327c82b7c825610b8a31de57
Steffen Planthaber <steffen.planthaber@dfki.de>
2022-02-17 07:48:40 +0100

    CLI: move functions to cpp file

--- examples/CLI/ConsoleCommands.cpp
+++ examples/CLI/ConsoleCommands.cpp
@@ -53,3 +53,54 @@ char** ConsoleCommands::attempted_completion_function(const char * text, int sta
     rl_attempted_completion_over = 1;
     return rl_completion_matches(text, &ConsoleCommands::match_finder);
 }
+
+
+void ConsoleCommands::registerCommand(const std::string &name, std::function<void(const std::vector<std::string> &params)> func, bool use_thread) {
+    CommandDef def;
+    def.func = func;
+    def.use_thread = use_thread;
+    commands[name] = def;
+}
+
+int ConsoleCommands::registerParamsForCommand(const std::string &name, const std::vector<ParamDef> &params) {
+    if (commands.find(name) != commands.end()) {
+        commands[name].params = params;
+        return true;
+    }
+    printf("unknown command %s\n\tyou need to register the command before adding paramater definitions\n", name.c_str());
+    return false;
+}
+
+void ConsoleCommands::runCommand(const std::string &name, std::vector<std::string> &params) {
+    std::string cmd = name;
+    if (name[name.size()-1] == ' ') {
+        cmd.erase(name.size()-1, 1);
+    }
+    auto iter = commands.find(cmd);
+    if (iter != commands.end()) {
+        while (params.size() < iter->second.params.size()) {
+            std::string question = iter->second.params[params.size()].hint + "[" + iter->second.params[params.size()].defaultvalue +"]:";
+            std::string param;
+            std::cout << "missing paramater: " << question;
+            std::getline(std::cin, param);
+            if (param == "") {
+                param = iter->second.params[params.size()].defaultvalue;
+            }
+            params.push_back(param);
+        }
+
+        if (iter->second.use_thread) {
+            thread_running = true;
+            cmdthread = std::thread([&](){
+                while (thread_running) {
+                    iter->second.func(params);
+                }
+            });
+            readline("press enter to stop\n");
+            thread_running = false;
+            cmdthread.join();
+        } else {
+            iter->second.func(params);
+        }
+    }
+}
