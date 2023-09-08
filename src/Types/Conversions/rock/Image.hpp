6a9af8f8a1272d1d77f1c32c673e8c16f8701c67
Christoph Hertzberg <christoph.hertzberg@dfki.de>
2023-09-08 14:26:47 +0200

    make code compile

--- src/Types/Conversions/rock/Image.hpp
+++ src/Types/Conversions/rock/Image.hpp
@@ -10,7 +10,15 @@
 namespace robot_remote_control {
 namespace RockConversion {
 
-    static const boost::bimap<std::string, base::samples::frame::frame_mode_t> encodings = {
+    // https://stackoverflow.com/a/31841462
+    template <typename L, typename R>
+    boost::bimap<L, R>
+    make_bimap(std::initializer_list<typename boost::bimap<L, R>::value_type> list)
+    {
+        return boost::bimap<L, R>(list.begin(), list.end());
+    }
+
+    static const boost::bimap<std::string, base::samples::frame::frame_mode_t> encodings = make_bimap<std::string, base::samples::frame::frame_mode_t>({
             {"MODE_UNDEFINED", base::samples::frame::MODE_UNDEFINED},
             {"MODE_BAYER", base::samples::frame::MODE_BAYER},
             {"MODE_BAYER_RGGB", base::samples::frame::MODE_BAYER_RGGB},
@@ -25,7 +33,7 @@ namespace RockConversion {
             {"MODE_PJPG", base::samples::frame::MODE_PJPG},
             {"MODE_JPEG", base::samples::frame::MODE_JPEG},
             {"MODE_PNG", base::samples::frame::MODE_PNG}
-    };
+    });
 
     // inline static void convert(const IMU &rrc_type, base::samples::IMUSensors* rock_type) {
     //     convert(rrc_type.acceleration(), &(rock_type->acc));
@@ -47,18 +55,18 @@ namespace RockConversion {
         rrc_type->set_data(std::string(reinterpret_cast<const char*>(rock_type.image.data()), rock_type.getNumberOfBytes()));
     }
 
-    inline static void convert(Image const &rrc_type, base::samples::frame::Frame *rock_type, ) {
+    inline static void convert(Image const &rrc_type, base::samples::frame::Frame *rock_type) {
 
-        convert(rrc_type.header().timestamp(), rock_type->time);
+        convert(rrc_type.header().timestamp(), &rock_type->time);
         auto const encoding_mode = encodings.left.find(rrc_type.encoding());
         base::samples::frame::frame_mode_t encoding = encoding_mode != encodings.left.end()
                 ? encoding_mode->second : base::samples::frame::MODE_UNDEFINED;
 
-        rock_type->init(rrc_type.width(), rrc_type.height(), 8, encoding, 0, rrc_type.data.size());
+        rock_type->init(rrc_type.width(), rrc_type.height(), 8, encoding, 0, rrc_type.data().size());
 
 //        rrc_type->set_step(rock_type.getRowSize());
 //        rrc_type->set_is_bigendian(true);
-        rock_type->setImage(rrc_type.data.data(), rrc_type.data().size());
+        rock_type->setImage(rrc_type.data().data(), rrc_type.data().size());
     }
 
 
