41f910974edc4c40da305905f3debd6d3a92408f
Steffen Planthaber <steffen.planthaber@dfki.de>
2024-11-14 16:35:02 +0100

    add SimpleActionHelper class

---
+++ src/Tools/SimpleActionHelper.cpp
@@ -0,0 +1,127 @@
+#include "SimpleActionHelper.hpp"
+
+
+
+
+SimpleActionsHelper::SimpleActionsHelper(const robot_remote_control::SimpleActions& initfrom) {
+    initFrom(initfrom);
+}
+
+void SimpleActionsHelper::initFrom(const robot_remote_control::SimpleActions& initfrom) {
+    actions.Clear();
+    actionbyname.clear();
+
+    actions.CopyFrom(initfrom);
+
+    //restore access buffers
+    for (auto& action : *actions.mutable_actions()) {
+        std::shared_ptr<SimpleActionWrapper> wrapper = std::make_shared<SimpleActionWrapper>(&action);
+        actionbyname[action.name()] = wrapper;
+
+        // restore valueByName access buffer
+        for (auto& namedValue : action.type().value_names()) {
+            wrapper->valueByName[namedValue.name()] = namedValue.value();
+            wrapper->nameByValue[namedValue.value()] = namedValue.name();
+        }
+        // restore actionDependencyStates access buffer
+        for (const auto & dep : action.type().action_dependency()) {
+            wrapper->actionDependencyStates[dep.depends_on_action()].push_back(dep.depends_on_action_in_state());
+        }
+    }
+}
+
+const robot_remote_control::SimpleActions& SimpleActionsHelper::getSimpleActionsDefinition() {
+    return actions;
+}
+
+std::shared_ptr<SimpleActionWrapper> SimpleActionsHelper::getSimpleAction(const std::string &actionname) {
+    return actionbyname[actionname];
+}
+
+float SimpleActionsHelper::getActionState(const std::string &actionname) {
+    return getSimpleAction(actionname)->getState();
+}
+
+void SimpleActionsHelper::setActionState(const std::string &actionname, const float &value) {
+    getSimpleAction(actionname)->setState(value);
+}
+
+std::shared_ptr<SimpleActionWrapper> SimpleActionsHelper::addSimpleAction(const std::string &actionname, const robot_remote_control::SimpleActionType& type) {
+    robot_remote_control::SimpleAction* action = actions.add_actions();
+    action->set_name(actionname);
+    action->mutable_type()->set_type(type);
+    std::shared_ptr<SimpleActionWrapper> wrapper = std::make_shared<SimpleActionWrapper>(action);
+    actionbyname[actionname] = wrapper;
+    return wrapper;
+}
+
+bool SimpleActionsHelper::canExecute(const std::shared_ptr<SimpleActionWrapper> &action) {    
+    //check dependencies (multiple entries on single actions: OR, between actions: AND)
+    for (const auto& actionstate : action->actionDependencyStates) {
+        bool has_match = false;
+        float curr_state = getActionState(actionstate.first);
+        //check or relation, if one is matching, the action has the correct state
+        for (const auto& state : actionstate.second) {
+            if (state ==  curr_state) {
+                has_match = true;
+            }
+        }
+        // if no state this action is not matching, action has unmet deps
+        if (!has_match) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+
+
+
+
+SimpleActionWrapper::SimpleActionWrapper(robot_remote_control::SimpleAction* action): action(action) {
+
+}
+
+void SimpleActionWrapper::setLimits(const float &min_state, const float &max_state, const float &step_size) {
+    action->mutable_type()->set_min_state(min_state);
+    action->mutable_type()->set_max_state(max_state);
+    action->mutable_type()->set_step_size(step_size);
+}
+
+/**
+ * @brief add named values to action as a 
+ * 
+ * @param valuenames 
+ */
+void SimpleActionWrapper::addNamedValues(const std::map<std::string, float> & valuenames) {
+    for (const auto& entry : valuenames) {
+        addNamedValue(entry.first, entry.second);
+    }
+}
+
+void SimpleActionWrapper::addNamedValue(const std::string &name, const float &value) {
+    robot_remote_control::NamedValue *nv = action->mutable_type()->add_value_names();
+    nv->set_name(name);
+    nv->set_value(value);
+    valueByName[name] = value;
+    nameByValue[value] = name;
+}
+
+float SimpleActionWrapper::getNamedValue(const std::string &valuename) {
+    return valueByName[valuename];
+}
+
+std::string SimpleActionWrapper::getValueName(const float &value) {
+    return nameByValue[value];
+}
+
+void SimpleActionWrapper::addActionDependency(const std::string &name, const float &value) {
+    robot_remote_control::ActionDependency* dep = action->mutable_type()->add_action_dependency();
+    dep->set_depends_on_action(name);
+    dep->set_depends_on_action_in_state(value);
+    actionDependencyStates[name].push_back(value);
+}
+
+
---
+++ test/test_SimpleActionHelper.cpp
@@ -0,0 +1,93 @@
+#include <boost/test/unit_test.hpp>
+
+#include "../src/Tools/SimpleActionHelper.hpp"
+#include "../src/MessageTypes.hpp"
+#include "../src/Types/RobotRemoteControl.pb.h"
+
+using namespace robot_remote_control;
+
+
+BOOST_AUTO_TEST_CASE(simple_action_helper) {
+   
+SimpleActionsHelper helper;
+
+std::shared_ptr<SimpleActionWrapper> action1 = helper.addSimpleAction("action1", robot_remote_control::VALUE_INT);
+std::shared_ptr<SimpleActionWrapper> action2 = helper.addSimpleAction("action2", robot_remote_control::VALUE_FLOAT);
+std::shared_ptr<SimpleActionWrapper> action3 = helper.addSimpleAction("action3", robot_remote_control::TRIGGER);
+
+
+BOOST_CHECK_EQUAL(action1->getType(), robot_remote_control::VALUE_INT);
+BOOST_CHECK_EQUAL(action2->getType(), robot_remote_control::VALUE_FLOAT);
+BOOST_CHECK_EQUAL(action3->getType(), robot_remote_control::TRIGGER);
+
+action1->setLimits(-1,3,1);
+BOOST_CHECK_EQUAL(action1->getAction().type().min_state(), -1);
+BOOST_CHECK_EQUAL(action1->getAction().type().max_state(), 3);
+BOOST_CHECK_EQUAL(action1->getAction().type().step_size(), 1);
+
+action1->addNamedValues({ {"VAL1", 1}, {"VAL2", 2},{"VAL3", 3} });
+BOOST_CHECK_EQUAL(action1->getAction().type().value_names(0).value(), 1);
+BOOST_CHECK_EQUAL(action1->getAction().type().value_names(1).value(), 2);
+BOOST_CHECK_EQUAL(action1->getAction().type().value_names(2).value(), 3);
+
+BOOST_CHECK_EQUAL(action1->getNamedValue("VAL1"), 1);
+BOOST_CHECK_EQUAL(action1->getNamedValue("VAL2"), 2);
+BOOST_CHECK_EQUAL(action1->getNamedValue("VAL3"), 3);
+
+BOOST_CHECK_EQUAL(action1->getValueName(1), "VAL1");
+BOOST_CHECK_EQUAL(action1->getValueName(2), "VAL2");
+BOOST_CHECK_EQUAL(action1->getValueName(3), "VAL3");
+
+
+action1->setState("VAL2");
+BOOST_CHECK_EQUAL(action1->getState(), 2);
+
+
+// helper.getSimpleActionsDefinition().PrintDebugString();
+
+action1->addActionDependency("action2", 3);
+action1->addActionDependency("action2", 4);// or relation
+
+BOOST_CHECK_EQUAL(helper.canExecute(action1), false);
+action2->setState(3);
+BOOST_CHECK_EQUAL(helper.canExecute(action1), true);
+action2->setState(4);
+BOOST_CHECK_EQUAL(helper.canExecute(action1), true);
+action2->setState(5);
+BOOST_CHECK_EQUAL(helper.canExecute(action1), false);
+
+// add a and dependency on another action
+action1->addActionDependency("action3", 1);
+BOOST_CHECK_EQUAL(helper.canExecute(action1), false);
+action2->setState(3);
+action3->setState(1);
+BOOST_CHECK_EQUAL(helper.canExecute(action1), true);
+action3->setState(0);
+BOOST_CHECK_EQUAL(helper.canExecute(action1), false);
+
+// actions without deps can always be executed
+BOOST_CHECK_EQUAL(helper.canExecute(action2), true);
+
+
+
+// check copy/restore
+SimpleActionsHelper helper2(helper.getSimpleActionsDefinition());
+
+BOOST_TEST(helper.getSimpleActionsDefinition().SerializeAsString() == helper2.getSimpleActionsDefinition().SerializeAsString());
+BOOST_TEST(helper.getSimpleAction("action1")->getAction().SerializeAsString() == helper2.getSimpleAction("action1")->getAction().SerializeAsString());
+BOOST_CHECK_EQUAL(helper.getSimpleAction("action1")->getNamedValue("VAL2"), helper2.getSimpleAction("action1")->getNamedValue("VAL2"));
+
+BOOST_CHECK_EQUAL(helper2.getSimpleAction("action1")->getType(), robot_remote_control::VALUE_INT);
+BOOST_CHECK_EQUAL(helper2.getSimpleAction("action2")->getType(), robot_remote_control::VALUE_FLOAT);
+BOOST_CHECK_EQUAL(helper2.getSimpleAction("action3")->getType(), robot_remote_control::TRIGGER);
+
+BOOST_CHECK_EQUAL(helper2.canExecute(action1), false);
+helper2.setActionState("action3", 1);
+BOOST_CHECK_EQUAL(helper2.canExecute(action1), true);
+
+// now we set a state on the copy, they are different
+BOOST_TEST(helper.getSimpleActionsDefinition().SerializeAsString() != helper2.getSimpleActionsDefinition().SerializeAsString());
+
+
+}
+
